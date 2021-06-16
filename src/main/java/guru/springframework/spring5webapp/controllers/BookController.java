d054fc714da8e32a99312fdde63f7be131b45cc8
hyuub <de3p27@gmail.com>
2021-06-16 17:59:54 +0200

    Exercise 3 - List of authors HTML

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -11,6 +11,7 @@ public class BookController {
     private final BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
+
         this.bookRepository = bookRepository;
     }
 
